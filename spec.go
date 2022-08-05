9b62f9cc9ef9d238167904c3db47df1e6e05df7b
xh3b4sd <xh3b4sd@gmail.com>
2022-08-05 14:46:44 +0200

    implement loader

--- spec.go
+++ spec.go
@@ -1,50 +1,59 @@
 package xgboost
 
-// Ensemble describes how boosters can be restored and used for predictions.
-// Creating a new ensemble might be as simple as shown below.
+type Fitter interface {
+	Models()
+	Single()
+}
+
+// Loader describes how boosters can be restored and used for predictions.
 //
-//     ens := &Booster{}
+//     ldr := &loader.Loader{
+//         Pat: "/Users/xh3b4sd/dat/",
+//         Por: 8080,
+//         Tem: "<template.py>",
+//     }
 //
-type Ensemble interface {
+type Loader interface {
 	// Restore loads an ensemble of layered models originally saved in universal
 	// binary JSON format via the "save_model" Python API of XGBoost. Suppose
 	// having trained and saved 3 models in Python, namely a, b and c.
 	//
-	//     a.save_model("a.ubj")
-	//     b.save_model("b.ubj")
-	//     c.save_model("c.ubj")
+	//     a.save_model("a.ubj") // buffer hash foo
+	//     b.save_model("b.ubj") // buffer hash bar
+	//     c.save_model("c.ubj") // buffer hash baz
 	//
 	// The 3 models saved above feed predictions into a combining ensemble e
 	// which we have trained and saved in Python as well.
 	//
 	//     e.save_model("e.ubj")
 	//
-	// A booster instance implementing the Ensemble interface based on our
-	// scenario described above can spawn a child process given the file paths
-	// to the respective model artefacts.
+	// A booster instance implementing the Loader interface based on our
+	// scenario described above can spawn a child process given the required
+	// data path, server port and script template.
 	//
-	//     err := ens.Restore("e.ubj", "a.ubj", "b.ubj", "c.ubj")
+	//     err := ldr.Restore()
 	//
 	// For more information on the IO Model of XGBoost see their official
 	// documentation.
 	//
 	//     https://xgboost.readthedocs.io/en/stable/tutorials/saving_model.html
 	//
-	Restore(string, ...string) error
+	Restore() error
 	// Predict can be called to gather predictions from the underlying ensemble
 	// once the ensemble instance got bootstrapped via Restore. As per our
-	// example from above, the input data would be lists of inputs per
-	// underlying model, ordered in training order.
+	// example from above, the input data would be a mapping of inputs per
+	// underlying model, where the map keys would resemble the buffer hashes of
+	// the trained models.
 	//
-	//     inp := [][]float32{
-	//         { ... }, // input for a.ubj
-	//         { ... }, // input for b.ubj
-	//         { ... }, // input for c.ubj
+	//     inp := map[string][]float32{
+	//         "foo": [ ... ], // features for a.ubj
+	//         "bar": [ ... ], // features for b.ubj
+	//         "baz": [ ... ], // features for c.ubj
 	//     }
 	//
 	// For a multiclass ensemble the returned prediction should yield the
 	// predicted class in numeric representation.
-	Predict([][]float32) (float32, error)
+	Predict(map[string][]float32) (float32, error)
 	// Sigkill shuts down the spawned child process. No predictions can be made
 	// anymore after calling Sigkill.
 	Sigkill() error
