b0d44ba141c6214ce9011e845cdc0f85ef89801c
xh3b4sd <xh3b4sd@gmail.com>
2022-08-08 15:11:38 +0200

    add model creation template

---
+++ model/exists.go
@@ -0,0 +1,14 @@
+package model
+
+import "os"
+
+func exists(file string) bool {
+	_, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		return false
+	} else if err != nil {
+		panic(err)
+	}
+
+	return true
+}
